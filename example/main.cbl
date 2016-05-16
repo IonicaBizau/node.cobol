d4e7236a809549eb9725bf5d51592bc57d505f27
Ionică Bizău <bizauionica@gmail.com>
2016-05-16 21:13:38 +0300

    Better examples

--- example/main.cbl
+++ example/main.cbl
@@ -1,4 +1,5 @@
-      * Compile this file together with the node.cobol modules:
+      * Compile this file together with the node.cobol
+      * modules:
       *
       *  $ cobc -x example/main.cbl lib/node-exec-*
       *
@@ -10,11 +11,16 @@
 
        DATA DIVISION.
           WORKING-STORAGE SECTION.
-          01 NODEJS-CODE PIC X(100) value "console.log('Hello!')".
+          01 NODEJS-CODE PIC X(100) value "console.log('Hello World!')".
 
        PROCEDURE DIVISION.
       * Execute a short Node.js snippet
            CALL 'EXEC_NODEJS' USING NODEJS-CODE
+
+           DISPLAY "Starting an HTTP server on port 8000".
+
+      * Convert an image into ASCII/ANSI art
+           CALL 'EXEC_NODEJS_FILE' USING "example/grace-hopper.js"
 
            DISPLAY "Starting an HTTP server on port 8000".
 
--- example/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const http = require("http");
-
-http.createServer((req, res) => {
-    res.end("Hello world from an Node.js http server executed in COBOL!");
-}).listen(8000);
