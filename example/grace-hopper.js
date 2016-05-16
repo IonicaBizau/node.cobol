892eaf5ae2c15374f15f22f4e7cc928bc44ff86a
Ionică Bizău <bizauionica@gmail.com>
2016-05-16 21:16:43 +0300

    Better screenshot

--- example/grace-hopper.js
+++ example/grace-hopper.js
@@ -1,3 +1,11 @@
+"use strict";
+
 const imgToAscii = require("image-to-ascii");
 
-imgToAscii("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg/220px-Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg", { bg: true }, (err, data) => console.log((err || data) + "\n                  * * * Rear Admiral Grace M. Hopper * * *"));
+imgToAscii(
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg/220px-Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg"
+  , { bg: true }
+  , (err, data) => console.log((err || data)
+  + `\n${" ".repeat(20)}* * * Rear Admiral Grace M. Hopper * * *`
+  )
+);
